938fbbba69b9a8a73db747ea2be5a31f2ad270c3
yusong <1193950650@qq.com>
2018-08-16 19:41:25 +0800

    代付款界面的编写

--- wexx/src/pages/dfk/index.vue
+++ wexx/src/pages/dfk/index.vue
@@ -4,10 +4,28 @@
 			<div class="weui-panel__bd">
 				<div class="weui-media-box weui-media-box_small-appmsg">
 					<div class="weui-cells weui-cells_in-small-appmsg">
-
+            <div class="fukuan">
+              <div>卖家ID</div>
+            </div>
 					</div>
 				</div>
 			</div>
+
+      <div>
+        <div>
+          <div>
+            <img src="" alt="">
+          </div>
+          <div>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, exercitationem?
+          </div>
+          <div>￥799.00</div>
+        </div>
+        <div>
+          <span>取消订单</span>
+          <span>付款</span>
+        </div>
+      </div>
 		</div>
 	</div>
 </template>
